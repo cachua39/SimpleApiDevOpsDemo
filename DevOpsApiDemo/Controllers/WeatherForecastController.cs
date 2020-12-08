6356a79f74cc3b0994b387d997f567d1b4598413
luonglv39 <luonglv39@gmail.com>
2020-12-08 14:27:56 +0700

    Edit list of summaries

--- DevOpsApiDemo/Controllers/WeatherForecastController.cs
+++ DevOpsApiDemo/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace DevOpsApiDemo.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezinggg", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezingg", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
